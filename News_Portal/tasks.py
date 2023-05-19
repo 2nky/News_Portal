553717044aa86b9e72dc622415f7a453d60f4149
2nky <121765538+2nky@users.noreply.github.com>
2023-05-19 13:23:58 +0200

    Добавлена рассылка новостей по расписанию

--- News_Portal/tasks.py
+++ News_Portal/tasks.py
@@ -1,8 +1,10 @@
+from datetime import datetime, timedelta
+
 from celery import shared_task
 from django.core.mail import send_mail
 from django.template.loader import render_to_string
 
-from News_Portal.models import Subscribers, Post
+from News_Portal.models import PostCategory, Subscribers, Post, Category
 from News_Portal.constants import OUR_SITE_URL
 
 
@@ -22,6 +24,36 @@ def send_news_notification(post_pk):
                     context={
                         "user": person.user,
                         "post": post,
+                        "site_url": OUR_SITE_URL,
+                    },
+                    request=None,
+                    using=None,
+                ),
+                from_email="pol9.f@yandex.ru",
+                recipient_list=[person.user.email],
+            )
+
+
+@shared_task
+def send_weekly_news():
+    categories = Category.objects.all()
+    now = datetime.now()
+    week_before = timedelta(weeks=1)
+
+    for category in categories:
+        posts = Post.objects.filter(
+            category=category, creation_time__gte=now - week_before
+        )
+        subscribers = Subscribers.objects.filter(category=category)
+        for person in subscribers:
+            send_mail(
+                subject="Новые статьи за неделю в любимом разделе",
+                message="",
+                html_message=render_to_string(
+                    "weekly_news.html",
+                    context={
+                        "user": person.user,
+                        "posts": posts,
                         "site_url": OUR_SITE_URL,
                     },
                     request=None,
