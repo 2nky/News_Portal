296f2a6b24a59e0e99da2b3ecedde7a26c42b2ca
2nky <121765538+2nky@users.noreply.github.com>
2023-06-28 21:30:36 +0200

    Добавлено настройки логгиирования согласно дз

--- NewsPaper/settings.py
+++ NewsPaper/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 import os
 from pathlib import Path
+import logging
+
+from django.core.mail import send_mail
 
 # Build paths inside the protect like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -24,7 +27,7 @@ SITE_ID = 1
 SECRET_KEY = "django-insecure-_v$&c)&1a2soq8*z#q20lm6_f$qfi*rshu$^9mgqm8%iwdfm8*"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["127.0.0.1"]
 
@@ -176,19 +179,148 @@ CACHES = {
 LOCALE_PATHS = [os.path.join(BASE_DIR, "locale")]
 
 # =============
+class MyEmailHandler(logging.Handler):
+    def __init__(self, level=logging.NOTSET, email=None):
+        if email is None:
+            raise ValueError("Email should be set!")
+
+        self.destination_email = email
+        super().__init__(level)
+
+    def emit(self, record) -> None:
+        send_mail(
+            f"Log record: {record.levelname}",
+            self.format(record),
+            SERVER_EMAIL,
+            ("Log Receiver", self.destination_email),
+        )
+
+
+class NoExceptionInfoFormatter(logging.Formatter):
+    def formatMessage(self, record) -> str:
+        record.exc_info = None
+        record.exc_text = None
+        return super().formatMessage(record)
+
+
+def filter_maker(level):
+    level = getattr(logging, level)
+
+    def filter(record):
+        return record.levelno <= level
+
+    return filter
+
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
 LOGGING = {
     "version": 1,
-    "disable_existing_loggers": False,
+    "disable_existing_loggers": True,
     "formatters": {
-        "debug_format": {"format": "%(asctime)s - %(levelname)s - %(message)s"}
+        "debug_fmt": {
+            "format": "%(asctime)s - %(levelname)s - %(message)s",
+        },
+        "warning_fmt": {
+            "format": "%(asctime)s - %(levelname)s - %(pathname)s - %(message)s",
+        },
+        "module_fmt": {
+            "format": "%(asctime)s - %(levelname)s - %(module)s - %(message)s"
+        },
+        "no_stack_trace_fmt": {
+            "format": "%(asctime)s - %(levelname)s - %(module)s - %(message)s",
+            "()": "NewsPaper.settings.NoExceptionInfoFormatter",
+        },
     },
-    "filters": {},
+    "filters": {
+        "debug_msg_only": {"level": "DEBUG", "()": "NewsPaper.settings.filter_maker"},
+        "require_debug_true": {
+            "()": "django.utils.log.RequireDebugTrue",
+        },
+        "require_debug_false": {
+            "()": "django.utils.log.RequireDebugFalse",
+        },
+    },
     "handlers": {
         "console_debug": {
             "level": "DEBUG",
             "class": "logging.StreamHandler",
-            "formatter": "debug_format",
-        }
+            "formatter": "debug_fmt",
+            "filters": ["debug_msg_only", "require_debug_true"],
+        },
+        "console_warning": {
+            "level": "WARNING",
+            "class": "logging.StreamHandler",
+            "formatter": "warning_fmt",
+            "filters": ["require_debug_true"],
+        },
+        "general_log": {
+            "level": "INFO",
+            "class": "logging.FileHandler",
+            "filename": "general.log",
+            "formatter": "no_stack_trace_fmt",
+            "filters": ["require_debug_false"],
+        },
+        "errors_log": {
+            "level": "INFO",
+            "class": "logging.FileHandler",
+            "filename": "errors.log",
+            "formatter": "module_fmt",
+        },
+        "security_log": {
+            "level": "DEBUG",
+            "class": "logging.FileHandler",
+            "filename": "security.log",
+            "formatter": "module_fmt",
+        },
+        "mail_admins": {
+            "level": "DEBUG",
+            "class": "NewsPaper.settings.MyEmailHandler",
+            "email": "pol9@list.ru",
+            "formatter": "no_stack_trace_fmt",
+        },
     },
-    "loggers": {"django": {"level": "DEBUG", "handlers": ["console_debug"]}},
+    "loggers": {
+        "django": {
+            "handlers": [
+                "console_debug",
+                "console_warning",
+                "general_log",
+            ],
+            "propagate": False,
+            "level": "DEBUG",
+        },
+        "django.request": {
+            "handlers": [
+                "mail_admins",
+                "errors_log",
+            ],
+            "level": "ERROR",
+        },
+        "django.server": {
+            "handlers": [
+                "errors_log",
+                "mail_admins",
+            ],
+            "level": "ERROR",
+        },
+        "django.template": {
+            "handlers": [
+                "errors_log",
+            ],
+            "level": "ERROR",
+        },
+        "django.db.backends": {
+            "handlers": [
+                "errors_log",
+            ],
+            "level": "ERROR",
+        },
+        "django.security": {
+            "handlers": [
+                "security_log",
+            ],
+            "level": "DEBUG",
+        },
+    },
 }
